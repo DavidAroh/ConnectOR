749f07a813ff16998699b6054881c2ee676b6b18
DavidAroh <arohdavid883@gmail.com>
2024-06-28 12:43:33 +0100

    almost done

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,6 @@
- // Function to navigate to the next page
  function goToNextPage() {
-    // Define the URL of the next page
-    const nextPageUrl = "./pages/reg.html"; // Update this URL as needed
-
-    // Redirect to the next page
+    const nextPageUrl = "./pages/reg.html"; 
     window.location.href = nextPageUrl;
 }
-
-// Set a timer to automatically navigate to the next page after a delay
-// Delay time is in milliseconds (e.g., 5000 ms = 5 seconds)
-const delayTime = 10000; // Adjust the delay time as needed
+const delayTime = 10000; 
 setTimeout(goToNextPage, delayTime);
