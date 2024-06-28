ff7ad2ef9a4bed981026f77b499eaba0ae2d5deb
DavidAroh <arohdavid883@gmail.com>
2024-06-28 13:01:38 +0100

    not done

---
+++ scripts/index2.js
@@ -0,0 +1,6 @@
+function goToNextPage() {
+    const nextPageUrl = "../pages/comm-section.html"; 
+    window.location.href = nextPageUrl;
+}
+const delayTime = 15000; 
+setTimeout(goToNextPage, delayTime);
